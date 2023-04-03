115d9b77af7f765ad7b534dfb367c0398c7c67d6
mandelamane <amachan3322@yahoo.co.jp>
2023-04-03 17:58:05 +0900

    add upload and download function

--- apps/vanishingmask/mask2face.py
+++ apps/vanishingmask/mask2face.py
@@ -5,9 +5,8 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 from tensorflow import keras
-from tensorflow.keras.utils import CustomObjectScope
 from tensorflow.keras.losses import mean_squared_error
-
+from tensorflow.keras.utils import CustomObjectScope
 
 
 def binary2array(img_binary):
@@ -30,7 +29,6 @@ def ssim_loss(gt, y_pred, max_val=1.0):
 def generate_face(img_binary, img_size=(256, 256)):
     img_array = binary2array(img_binary)
 
-
     img_shape = img_array.shape
     height, width = img_shape[0], img_shape[1]
 
@@ -40,27 +38,25 @@ def generate_face(img_binary, img_size=(256, 256)):
     img_array = img_array[:, cut_index1:cut_index2]
     img_resized = cv2.resize(img_array, img_size)
 
-    img_normalized = img_resized[:,:,::-1].astype(np.float32) / 255.0
-    # img_normalized = img_resized.astype("float32") / 255.0
+    img_normalized = img_resized[:, :, ::-1].astype(np.float32) / 255.0
     img_normalized = img_normalized[np.newaxis, :, :, :]
 
+    with CustomObjectScope(
+        {"ssim_loss": ssim_loss, "ssim_l1_loss": ssim_l1_loss}
+    ):
+        model = keras.models.load_model(
+            "apps/vanishingmask/models/mask2face_mega.h5"
+        )
 
-    with CustomObjectScope({'ssim_loss': ssim_loss, 'ssim_l1_loss': ssim_l1_loss}):
-    # with CustomObjectScope({'ssim_loss': ssim_loss}):
-            model = keras.models.load_model(
-        "apps/vanishingmask/models/mask2face_mega.h5"
-        # "apps/vanishingmask/models/unet_model.h5"
-    )
-    print(model.summary())
-    tf.keras.utils.plot_model(model, show_shapes=True, expand_nested=True, to_file="./model.png")
     gen_face_array = model.predict(img_normalized)[0][:, :, ::-1]
-    # gen_face_array = model.predict(img_normalized)[0]
 
     gen_face_array_resize = cv2.resize(gen_face_array, (height, height))
     gen_face_array = np.append(
         np.ones((height, cut_index1, 3)), gen_face_array_resize, axis=1
     )
-    gen_face_array = np.append(gen_face_array, np.ones((height, cut_index1, 3)), axis=1)
+    gen_face_array = np.append(
+        gen_face_array, np.ones((height, cut_index1, 3)), axis=1
+    )
 
     img_buffer = io.BytesIO()
 
--- apps/vanishingmask/static/capture.js
+++ apps/vanishingmask/static/capture.js
@@ -2,13 +2,22 @@ import Button from "./button.js";
 import * as video from "./video.js";
 
 let blob = null;
+let m2f_blob = null;
+let uploadInput = null;
 let takePictureButton = null;
 let vanishButton = null;
 let resetButton = null;
+let downloadButton = null;
 
 function startup() {
     video.init();
-    takePictureButton = new Button(document.querySelector("#startbutton"), function() {
+    uploadInput = document.querySelector('#fileinput');
+    uploadInput.addEventListener("change", function (ev) {
+        takePictureButton.disable();
+        vanishButton.enable();
+        changeImage(ev);
+    });
+    takePictureButton = new Button(document.querySelector("#startbutton"), function () {
         takePictureButton.disable();
         video.takeScreenShot().then(b => {
             blob = b;
@@ -16,29 +25,76 @@ function startup() {
             vanishButton.enable();
         })
     });
-    vanishButton = new Button(document.querySelector("#vanishbutton"), function() {
+    vanishButton = new Button(document.querySelector("#vanishbutton"), function () {
         vanishButton.disable();
+        uploadInput.disabled = true;
         vanishmask(blob).then(blob => {
+            m2f_blob = blob;
             video.showPicture(blob);
+            downloadButton.enable();
         })
     });
-    resetButton = new Button(document.querySelector("#resetbutton"), function() {
+    resetButton = new Button(document.querySelector("#resetbutton"), function () {
         reset();
+    });
+    downloadButton = new Button(document.querySelector("#downloadbutton"), function () {
+        download(m2f_blob);
     });
     reset();
 }
 
 async function vanishmask(blob) {
-    const  formdata = new FormData();
+    const formdata = new FormData();
     formdata.set("picture", blob);
     return await (await fetch('/upload', { method: "POST", body: formdata })).blob();
 }
 
+function download(blob) {
+    if (blob) {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.download = 'mask2face.png';
+        a.href = url;
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(url);
+    }
+}
+
+function changeImage(ev) {
+    const file = ev.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        const dataUrl = reader.result;
+        blob = dataUrlToBlob(dataUrl);
+        video.showPicture(blob);
+    });
+    reader.readAsDataURL(file);
+}
+
+function dataUrlToBlob(dataUrl) {
+    const parts = dataUrl.split(';base64,');
+    const contentType = parts[0].split(':')[1];
+    const raw = window.atob(parts[1]);
+    const rawLength = raw.length;
+    const uInt8Array = new Uint8Array(rawLength);
+
+    for (let i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], { type: contentType });
+}
+
 function reset() {
     blob = null;
+    m2f_blob = null
+    uploadInput.disabled = false;
     takePictureButton.enable();
     vanishButton.disable();
     resetButton.enable();
+    downloadButton.disable();
     video.showVideo();
 }
 
